cc6963241d9b12e47c50b673fb6408ad65a9a1ff
José Victor <josevictorgaspari@gmail.com>
2021-07-27 17:27:56 -0300

    segunda aula

---
+++ src/components/commons/Menu/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Logo from "../../../theme/Logo/Index";
+import { MenuWrapper } from "./styles/MenuWrapper";
+
+const links = [
+    {
+        texto: 'Home',
+        url: '/'
+    },
+    {
+        texto: 'Perguntas frequentes',
+        url: '/faq'
+    },
+    {
+        texto: 'Sobre',
+        url: '/sobre'
+    },
+]
+
+export default function Menu() {
+    return(
+    <MenuWrapper>
+        <MenuWrapper.LeftSide>
+            <Logo/>
+        </MenuWrapper.LeftSide>
+        <MenuWrapper.CentralSide>
+            {links.map(function (link) {
+                return (
+                    <li>
+                        <a href={link.url}>
+                            {link.texto}
+                        </a>
+                    </li>
+                )
+            })}
+        </MenuWrapper.CentralSide>
+        <MenuWrapper.RightSide>
+            <button>
+                Entrar
+            </button>
+            <button>
+                Cadastrar
+            </button>
+        </MenuWrapper.RightSide>
+    </MenuWrapper>
+    )
+}
