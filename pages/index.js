9d3fcb9fcdb5c323540b707d2e23b5d8e1b2e927
José Victor <josevictorgaspari@gmail.com>
2021-07-28 15:21:15 -0300

    adicao do footer

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
+import Footer from "../src/components/commons/Footer";
 import Menu from "../src/components/commons/Menu";
  
 export default function Home() {
   return (
-    <div>
+    //essa estilização da div faz o footer ficar na parte de baixo da página
+    <div style={{
+      flex: '1',
+      display: 'flex',
+      flexWrap: 'wrap',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    }}>
       <Menu/>
+      <Footer/>
     </div>
   )
 }
